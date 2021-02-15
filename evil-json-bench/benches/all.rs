787a6bafa9d790292e0e6dc07adbfe09b193233c
Kogia-sima <orcinus4627@gmail.com>
2021-02-16 08:07:24 +0900

    fix throughput calculation

--- evil-json-bench/benches/all.rs
+++ evil-json-bench/benches/all.rs
@@ -20,14 +20,23 @@ fn citm_catalog(c: &mut Criterion) {
     let citm_catalog: CitmCatalog =
         serde_json::from_str(citm_catalog_str.as_str()).unwrap();
 
+    group.throughput(Throughput::Bytes(
+        serde_json::to_string(&citm_catalog).unwrap().len() as u64
+    ));
     group.bench_function("serde-json", |b| {
         b.iter(|| serde_json::to_string(&citm_catalog))
     });
 
+    group.throughput(Throughput::Bytes(
+        simd_json::to_string(&citm_catalog).unwrap().len() as u64
+    ));
     group.bench_function("simd-json", |b| {
         b.iter(|| simd_json::to_string(&citm_catalog))
     });
 
+    group.throughput(Throughput::Bytes(
+        evil_json::to_string(&citm_catalog).unwrap().len() as u64
+    ));
     group.bench_function("evil-json", |b| {
         b.iter(|| evil_json::to_string(&citm_catalog))
     });
@@ -36,23 +45,30 @@ fn citm_catalog(c: &mut Criterion) {
 }
 
 fn twitter(c: &mut Criterion) {
-    let twitter_str =
-        get_json("twitter").expect("Failed to read twitter.json");
+    let twitter_str = get_json("twitter").expect("Failed to read twitter.json");
 
     let mut group = c.benchmark_group("twitter.json");
     group.throughput(Throughput::Bytes(twitter_str.len() as u64));
 
-    let twitter: Twitter =
-        serde_json::from_str(twitter_str.as_str()).unwrap();
+    let twitter: Twitter = serde_json::from_str(twitter_str.as_str()).unwrap();
 
+    group.throughput(Throughput::Bytes(
+        serde_json::to_string(&twitter).unwrap().len() as u64
+    ));
     group.bench_function("serde-json", |b| {
         b.iter(|| serde_json::to_string(&twitter))
     });
 
+    group.throughput(Throughput::Bytes(
+        simd_json::to_string(&twitter).unwrap().len() as u64
+    ));
     group.bench_function("simd-json", |b| {
         b.iter(|| simd_json::to_string(&twitter))
     });
 
+    group.throughput(Throughput::Bytes(
+        evil_json::to_string(&twitter).unwrap().len() as u64
+    ));
     group.bench_function("evil-json", |b| {
         b.iter(|| evil_json::to_string(&twitter))
     });
